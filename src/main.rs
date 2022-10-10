32b6a317320bfb989cec78288ea3ba7279dfe6ee
patatahooligan <filwn.oikonomou@gmail.com>
2022-10-10 14:26:58 +0300

    Make the binary freestanding
    This means that it can run with no runtime. Specifically, this requires
    the following changes:
    
     - Don't link to the std crate.
     - Don't use "main". While main is the entry point of our application,
       it is not the real starting point of the application. It is normally
       preceded by a runtime that performs any required initialization. To
       make an operating system, we can't have a runtime to prepare stuff
       for us. So disable the entire "main" mechanism and define our own
       "_start" function.
     - Define a custom panic handler. Rust's panic handler is part of the
       std crate, which we are not using. Our current panic handler just
       freezes the system.
     - Disable stack unwinding by setting "panic" to "abort" in Cargo.toml.
       Stack unwinding is complicated and we won't need it for a very long
       time (ever?).
    
    Currently, this doesn't build without downloading and specifying a
    target triple that has no underlying operating system. We will set this
    up to be a default, but for now this can build with something like:
    
        rustup target add thumbv7em-none-eabihf
        cargo build --target thumbv7em-none-eabihf

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+// For an operating system we won't have access to the std library or a
+// runtime. Without a runtime, we also can't use the normal entry point
+// of "main", see _start() below.
+#![no_main]
+#![no_std]
+
+use core::panic::PanicInfo;
+
+// Disable mangling because we need the linker to see this as exactly
+// "_start". For similar reasons, use the C calling convention. The C
+// linker looks for _start by default, so we don't have to explicitly
+// state that this is the entry point.
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+/// Custom panic handler. This is a requirement for no_std. We can't do
+/// something truly meaningful at this time. Just loop forever, ie
+/// freeze the system.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
