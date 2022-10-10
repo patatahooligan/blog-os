584bd0f99b3f409bc6ac510a2b71b6598fc93a06
patatahooligan <filwn.oikonomou@gmail.com>
2022-10-10 15:43:23 +0300

    Set up project to automatically run in qemu
    Also add simple "Hello World!" functionality to `_start` in order to
    test the result. It uses the VGA buffer which is compatible with a huge
    range of machines.
    
    So far we had a standalone executable but it wasn't really bootable. We
    use the `bootloader` crate and the `bootimage` tool to create a bootable
    image. We also set up `cargo run` to run the produced image through qemu
    for easy testing.
    
    Note that apparently, newer versions of `bootloader` do not require
    `bootimage`, but I don't want to mess with stuff that is not in the
    tutorial yet. It is a good idea for a future improvement after I'm done
    with the main part of the tutorial.

--- README.md
+++ README.md
@@ -3,3 +3,31 @@
 This is my implementation of Philipp Oppermann's Blog OS, a small
 operating system written in Rust. The original material I'm following
 can currently be found at https://os.phil-opp.com/ .
+
+# Building
+
+Currently, this requires some unstable features from the nightly branch
+of Rust. If you are using `rustup`, you can install it with
+
+    rustup install nightly
+
+The project specifies that it requires the nightly branch, so you
+shouldn't have to set it as your system default or anything.
+
+Additionally, to build a bootable image we require the `bootimage` tool
+and the `llvm-tools-preview` component.
+
+    cargo install bootimage
+    rustup component add llvm-tools-preview
+
+Then, to create a bootable image, run
+
+    cargo bootimage
+
+# Running
+
+You can (probably) write the bootable image to USB and boot into it. But
+a more practical way to run this while testing/developing is to use
+qemu. Make sure you have qemu installed for x86_64 and then simply run
+
+    cargo run
--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,30 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
+
+/// Print "Hello World!" using the VGA buffer. This is a toy _start
+/// function to simply have something to test.
 // Disable mangling because we need the linker to see this as exactly
 // "_start". For similar reasons, use the C calling convention. The C
 // linker looks for _start by default, so we don't have to explicitly
 // state that this is the entry point.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    // The VGA standard guarantees that we have a VGA buffer at address
+    // 0xb8000 that is a 80x25 array of (ASCII code, color) byte tuples.
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        // TODO: move the unsafe part to some wrapper class.
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    // Since our executable is an OS, it can't simply exit. Looping
+    // indefinitely is a way to "stop" when we're done.
     loop {}
 }
 
