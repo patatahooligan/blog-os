a774c7806f3f690963218ce1601144555fc7eacc
patatahooligan <filwn.oikonomou@gmail.com>
2022-10-12 01:26:36 +0300

    Remove example frame mapping
    It was only useful to develop the frame allocator. We don't actually
    need such an unsafe and hardcoded implementation.

--- src/main.rs
+++ src/main.rs
@@ -19,27 +19,10 @@ use core::panic::PanicInfo;
 
 entry_point!(kernel_main);
 
-fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use blog_os::memory;
-    use x86_64::structures::paging::Page;
-    use x86_64::VirtAddr;
-
+fn kernel_main(_boot_info: &'static BootInfo) -> ! {
     println!("Hello {}!", "world");
 
     blog_os::init();
-
-    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    let mut frame_allocator =
-        unsafe { memory::BootInfoFrameAllocator::init(&boot_info.memory_map) };
-
-    let page = Page::containing_address(VirtAddr::new(0xdeadbeef));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
-
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe {
-        page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e);
-    }
 
     #[cfg(test)]
     test_main();
