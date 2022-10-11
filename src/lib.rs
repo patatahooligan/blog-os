3b3587983b9e1a9359a6c3ac998c4fe9d1dfe82c
patatahooligan <filwn.oikonomou@gmail.com>
2022-10-12 02:05:35 +0300

    Implement heap allocation
    Offload most of the work to the `linked_list_allocator` crate so that we
    can create the surrounding infrastructure. The next step will be to
    replace the create with our own allocator.

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,13 @@
 #![cfg_attr(test, no_main)]
 #![feature(custom_test_frameworks)]
 #![feature(abi_x86_interrupt)]
+#![feature(alloc_error_handler)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+extern crate alloc;
+
+pub mod allocator;
 pub mod gdt;
 pub mod interrupts;
 pub mod memory;
@@ -30,6 +34,11 @@ pub fn hlt_loop() -> ! {
     loop {
         x86_64::instructions::hlt();
     }
+}
+
+#[alloc_error_handler]
+fn alloc_error_handler(layout: alloc::alloc::Layout) -> ! {
+    panic!("Allocation error: {:?}", layout)
 }
 
 #[cfg(test)]
